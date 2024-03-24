9390865cf7812b6a10fe8d4db5c952a1c99beba4
Stack Soumik <sarkarsoumik215@gmail.com>
2024-03-24 04:33:59 -0700

    fix/payment-page

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -1,48 +1,12 @@
+import { invvoic } from "@/src/components/data";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { BiMicrophone } from "react-icons/bi";
+import { BiMicrophone, BiPen } from "react-icons/bi";
 import { LuPrinter } from "react-icons/lu";
 
 export default function page() {
-  const invvoic = [
-    {
-      name: "Vitamin A Night Serum",
-      Rate: "$50$",
-      Qty: "2",
-      Amount: "$500",
-    },
-    {
-      name: "Vitamin A Night Serum",
-      Rate: "$50$",
-      Qty: "2",
-      Amount: "$500",
-    },
-    {
-      name: "Vitamin A Night Serum",
-      Rate: "$50$",
-      Qty: "2",
-      Amount: "$500",
-    },
-    {
-      name: "Vitamin A Night Serum",
-      Rate: "$50$",
-      Qty: "2",
-      Amount: "$500",
-    },
-    {
-      name: "Vitamin A Night Serum",
-      Rate: "$50$",
-      Qty: "2",
-      Amount: "$500",
-    },
-    {
-      name: "Vitamin A Night Serum",
-      Rate: "$50$",
-      Qty: "2",
-      Amount: "$500",
-    },
-  ];
+ 
   return (
     <>
       <div className=" pb-5">
@@ -54,7 +18,7 @@ export default function page() {
       <div className=" relative grid grid-cols-1 lg:grid-cols-3 ">
         <div className=" lg:col-start-1 lg:col-end-3  lg:absolute -left-2 mb-10 ">
           <div
-            className=" container bg-white lg:min-w-[750px] rounded-xl lg:px-6 lg:py-5 px-4 py-5
+            className=" container bg-white lg:max-w-[750px] rounded-xl lg:px-6 lg:py-5 px-4 py-5
            space-y-5 lg:space-y-10"
           >
             <h1 className="   text-sm sm:text-sm md:text-lg  lg:text-xl">
@@ -62,7 +26,7 @@ export default function page() {
             </h1>
 
             <div className=" flex  items-center justify-between gap-4">
-              <div className=" flex items-center justify-center lg:gap-3">
+              <div className=" flex items-center justify-center lg:gap-3 md:gap-2 gap-[2px]">
                 <input type="checkbox" placeholder="sd" className=" rounded-" />
                 <Link
                   href={"/payment/payment-method"}
@@ -94,7 +58,7 @@ export default function page() {
                   <h1 className=" opacity-50">Enter the name</h1>
                   <input
                     type="text"
-                    className="outline-none border lg:px-14 px-4 py-2 rounded-md"
+                    className="outline-none border lg:px-10 px-4 py-2 rounded-md"
                     name=""
                     id=""
                   />
@@ -103,7 +67,7 @@ export default function page() {
                   <h1 className=" opacity-50">Card Number</h1>
                   <input
                     type="text"
-                    className="outline-none border lg:px-14 px-4 py-2 rounded-md"
+                    className="outline-none border lg:px-10 px-4 py-2 rounded-md"
                     name=""
                     id=""
                   />
@@ -114,7 +78,7 @@ export default function page() {
                   <h1 className=" opacity-50">Expiry (MM/YY)</h1>
                   <input
                     type="text"
-                    className="outline-none border lg:px-14 px-4 py-2 rounded-md"
+                    className="outline-none border lg:px-10 px-4 py-2 rounded-md"
                     name=""
                     id=""
                   />
@@ -123,7 +87,7 @@ export default function page() {
                   <h1 className=" opacity-50">CVV Code</h1>
                   <input
                     type="text"
-                    className="outline-none border lg:px-14 px-4 py-2 rounded-md"
+                    className="outline-none border lg:px-10 px-4 py-2 rounded-md"
                     name=""
                     id=""
                   />
@@ -152,11 +116,15 @@ export default function page() {
         {/* right side */}
         <div className=" lg:col-start-3 lg:absolute lg:-left-[22px]">
           <div className=" container lg:min-w-[450px] mx-auto bg-white rounded-xl  px-5 py-5">
-            <div className=" py-8 border px-2 rounded-2xl">
+            <div className=" lg:py-8 py-3 px-2 border lg:px-5 rounded-2xl">
               <div className="  flex items-center justify-between ">
                 <h1> Preview Invoice</h1>
-                <div className="   p-3 md:p-4 lg:p-5 rounded-2xl bg-[#F8F8F8]">
+                <div className="flex  gap-10   p-3 md:p-4 lg:p-5 rounded-2xl">
                   <LuPrinter />
+
+                  <Link href={"/edit-invoice"} className="bg-[#F8F8F8]">
+                    <BiPen />
+                  </Link>
                 </div>
               </div>
               <div className=" mt-10">
@@ -171,20 +139,20 @@ export default function page() {
                 <h1>Dr. Vihang</h1>
                 <h1>Jane Doe</h1>
               </div>
-              <div className=" flex items-center justify-center flex-1 flex-col">
-                <table>
-                  <thead className=" bg-black  rounded-md text-white ">
+              <div className=" flex items-end justify-end rounded-md flex-1 flex-col">
+                <table className="min-w-full border rounded-xl">
+                  <thead className=" bg-black    text-white ">
                     <tr className=" py-10   rounded-md">
-                      <th className="px-5 py-3 border-b-2  text-left text-xs font-semibold uppercase tracking-wider">
+                      <th className=" px-2 py-3  rounded-tl-xl   md:px-5 md:py-3 border-b-2  text-left text-xs font-semibold uppercase tracking-wider">
                         item
                       </th>
-                      <th className="px-5 py-3 border-b-2  text-left text-xs font-semibold uppercase tracking-wider">
+                      <th className=" px-2 py-3   md:px-5 md:py-3 border-b-2  text-left text-xs font-semibold uppercase tracking-wider">
                         Rate
                       </th>
-                      <th className="px-5 py-3 border-b-2  text-left text-xs font-semibold uppercase tracking-wider">
+                      <th className=" px-2 py-3   md:px-5 md:py-3 border-b-2  text-left text-xs font-semibold uppercase tracking-wider">
                         Qty
                       </th>
-                      <th className="px-5 py-3 border-b-2  text-left text-xs font-semibold uppercase tracking-wider">
+                      <th className=" px-2 py-3 rounded-tr-xl  md:px-5 md:py-3 border-b-2  text-left text-xs font-semibold uppercase tracking-wider">
                         Amount
                       </th>
                     </tr>
@@ -192,26 +160,26 @@ export default function page() {
                   <tbody className=" ">
                     {invvoic.map((data, index) => (
                       <tr key={index}>
-                        <td className="px-5 py-5 border-b border-gray-100 bg-white text-sm">
+                        <td className="lg:px-5 lg:py-3  sm:px-3 sm:py-3 px-2 py-2  border-gray-200 bg-white text-sm">
                           <div className="flex items-center">
-                            <div className="ml-1">
+                            <div className="lg:ml-1">
                               <p className="text-gray-900 whitespace-no-wrap">
                                 {data.name}
                               </p>
                             </div>
                           </div>
                         </td>
-                        <td className="px-5 py-5 border-b border-gray-100 bg-white text-sm">
+                        <td className="lg:px-5 lg:py-3  sm:px-3 sm:py-3 px-2 py-2 border-gray-200 bg-white text-sm">
                           <p className="text-gray-900 whitespace-no-wrap">
                             {data.Rate}
                           </p>
                         </td>
-                        <td className="px-5 py-5 border-b border-gray-100 bg-white text-sm">
+                        <td className="lg:px-5 lg:py-3  sm:px-3 sm:py-3 px-2 py-2  border-gray-200 bg-white text-sm">
                           <p className="text-gray-900 whitespace-no-wrap">
                             {data.Qty}
                           </p>
                         </td>
-                        <td className="px-5 py-5 border-b border-gray-100 bg-white text-sm">
+                        <td className="lg:px-5 lg:py-3  sm:px-3 sm:py-3 px-2 py-2  border-gray-200 bg-white text-sm">
                           <p className="text-gray-900 whitespace-no-wrap">
                             {data.Amount}
                           </p>
@@ -220,7 +188,8 @@ export default function page() {
                     ))}
                   </tbody>
                 </table>
-                <div className="flex items-end justify-end   my-5 gap-20">
+ 
+                <div className="flex  my-5 gap-20">
                   <div className=" space-y-2">
                     <h3 className=" text-balance font-semibold">Subtotal</h3>
                     <h3>tax 5%</h3>
@@ -233,7 +202,10 @@ export default function page() {
                     <h3>$107</h3>
                   </div>
                 </div>
-                <div className=" p-5">
+                <div className=" hidden lg:block my-2 text-right bg-black/50 w-full h-[1px]">
+                  Total <span className=" ml-[140px]"> $500</span>
+                </div>
+                <div className=" min-w-full lg:px-5 lg:mt-5 py-3 px-2 rounded-md bg-[#F8F8F8]">
                   <h1 className="  font-bold">Notes/Terms</h1>
                   <p className=" opacity-50">
                     Lorem ipsum dolor sit amet. Sit eaque assumenda qui.
@@ -243,6 +215,7 @@ export default function page() {
             </div>
           </div>
         </div>
+
       </div>
     </>
   );
